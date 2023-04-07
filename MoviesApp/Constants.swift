9f3dd2e0c88a6cf243c081268c9f0dfaac474202
suhaKarakaya <suhabskarakaya@gmail.com>
2023-04-07 16:42:21 +0300

    ++

--- MoviesApp/Constants.swift
+++ MoviesApp/Constants.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class Constants {
-    static let apiKey:String =  ""
-    static let baseURL:String = ""
+    static let apiKey:String =  "eb2dfe5fb23e1bdcd3dbf005750c38be"
+    static let baseURL:String = "https://api.themoviedb.org/3/"
 }
+
